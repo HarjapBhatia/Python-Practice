20c0bc61cbdeb767be0f512fc5dd54a408f70bf3
Harjap <harjapbhatia3@gmail.com>
2025-07-10 15:50:46 +0530

    Add files via upload

---
+++ fileoperation.py
@@ -0,0 +1,14 @@
+# oepning and reading the file content
+
+# x = open("new.txt","r")
+# y = x.read()
+# print(y)
+# x.close()
+
+#
+x = open("new.txt","r")
+y1 = x.readline()
+y2 = x.readline()
+y3 = x.readline()
+print(y1 + y3)
+x.close()
